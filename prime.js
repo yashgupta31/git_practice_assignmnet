5dd839feea7998b8514849778021217b0d362acc
yashgupta31 <yashsntoshgupta2019@gmail.com>
2024-02-06 23:24:46 +0530

    upload by yash

---
+++ prime.js
@@ -0,0 +1,15 @@
+
+let num=5;
+let count=0;
+    for(let i=1; i<=num; i++){
+        if(num%i==0){
+            count++
+        }
+    }
+    
+    if(count==5){
+        console.log("Yes")
+    }
+    else{
+        console.log("No")
+    }
